f4c597f379c84c529e4d4b476c55a438ebbdbdd6
Nafis <abdullahnafis256@gmail.com>
2025-11-25 17:59:38 +0600

    navbar links modified and hover added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
       <span className="text-xl font-bold tracking-wide p-3"> &lt; &gt;<span className='text-[#6C63FF]'>FRONTEND DEV</span>&lt;/&gt;</span>
 
       <ul className="flex flex-col md:flex-row gap-4 text-lg w-full md:w-auto md:items-center">
-        <li><a href="#About" className="block border text-center md:border-0">About</a></li>
-        <li><a href="#Experience" className="block border text-center md:border-0">Experience</a></li>
-        <li><a href="#Projects" className="block border text-center md:border-0">Projects</a></li>
-        <li><a href="#Footer" className="block border text-center md:border-0">Contact</a></li>
+        <li><a href="#About" className="block border text-center md:border-0 hover:text-[20px] hover:text-[#6C63FF]">About</a></li>
+        <li><a href="#Experience" className="block border text-center md:border-0 hover:text-[20px] hover:text-[#6C63FF]">Experience</a></li>
+        <li><a href="#Projects" className="block border text-center md:border-0 hover:text-[20px] hover:text-[#6C63FF]">Projects</a></li>
+        <li><a href="#Footer" className="block border text-center md:border-0 hover:text-[20px] hover:text-[#6C63FF]">Contact</a></li>
       </ul>
 
     </nav>
