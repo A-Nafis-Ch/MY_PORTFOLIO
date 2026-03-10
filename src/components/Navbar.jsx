3793e40b8d5b631315cfa518e4f60718c42856a0
Nafis <abdullahnafis256@gmail.com>
2026-03-10 11:08:44 +0600

    Navbar  component bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="text-white flex flex-col md:flex-row md:justify-between p-4">
       <span className="text-xl font-bold tracking-wide p-3">
         {" "}
-        &lt; &gt;<span className="text-[#6C63FF]">FRONTEND DEV</span>&lt;/&gt;
+        &lt; &gt;<span className="text-[#6C63FF]">Full Stack DEV</span>&lt;/&gt;
       </span>
 
       <ul className="flex flex-col md:flex-row gap-4 text-lg w-full md:w-auto md:items-center">
